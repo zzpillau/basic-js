136c3308f887e10414b901251b65c493c980876e
zzpillau <zzpillau@gmail.com>
2025-10-20 08:59:45 +0200

    solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,29 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const validateSampleActivity = (sa) => {
+    const activity = Number(sa);
+    if (
+      typeof sa !== 'string' ||
+      isNaN(activity) ||
+      activity <= 0 ||
+      activity > MODERN_ACTIVITY
+    ) {
+      return false
+    }
+    return activity
+  }
+
+  const activity = validateSampleActivity(sampleActivity)
+
+  if (activity !== false) {
+    const k = Math.LN2 / HALF_LIFE_PERIOD;
+    const age = Math.log(MODERN_ACTIVITY / activity) / k;
+    return Math.ceil(age);
+  }
+    
+  return false
 }
 
 module.exports = {
