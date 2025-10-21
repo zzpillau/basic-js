9d03291d2c6a7df588c1a6ecb6a72956395f8170
zzpillau <zzpillau@gmail.com>
2025-10-21 21:16:14 +0200

    feat: solve mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rowsCount = matrix.length
+  const cellsCount = matrix[0].length
+
+  const field = Array.from({length: rowsCount}, () => Array.from({length: cellsCount}, () => 0))
+
+  matrix.forEach((row, i) => {
+    row.forEach((cell, j) => {
+
+      if (cell === true) {
+        const hasTop = i > 0
+        const hasBottom = i < (matrix.length - 1)
+        const hasRight = j < (row.length - 1)
+        const hasLeft = j > 0
+
+        hasTop && (field[i - 1][j] += 1 )
+
+        hasBottom && (field[i + 1][j] += 1)
+
+        hasRight && (field[i][j + 1] += 1)
+
+        hasLeft && (field[i][j - 1] += 1)
+
+        hasTop && hasRight && (field[i - 1][j + 1] += 1)
+
+        hasTop && hasLeft && (field[i - 1][j - 1] += 1)
+
+        hasBottom && hasRight && (field[i + 1][j + 1] += 1)
+
+        hasBottom && hasLeft && (field[i + 1][j - 1] += 1)
+      }
+    })
+  })
+
+  return field
 }
 
 module.exports = {
   minesweeper
 };
+
